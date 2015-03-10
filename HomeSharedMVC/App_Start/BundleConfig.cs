cdc53c15d5e5fa973f850fb9b542918a118f11ea
GlobusCrea <globuscrea@gmail.com>
2015-03-10 12:31:14 +0100

    Integration Layout OneStep

--- HomeSharedMVC/App_Start/BundleConfig.cs
+++ HomeSharedMVC/App_Start/BundleConfig.cs
@@ -8,6 +8,23 @@ namespace HomeSharedMVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            /* BUNDLE CSS*/
+            //<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" />
+            //<link rel="stylesheet" href="assets/style.css" />
+            //<link rel="stylesheet" href="assets/owl-carousel/owl.carousel.css">
+            //<link rel="stylesheet" href="assets/owl-carousel/owl.theme.css">
+            //<link rel="stylesheet" type="text/css" href="assets/slitslider/css/style.css" />
+            //<link rel="stylesheet" type="text/css" href="assets/slitslider/css/custom.css" />
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/style.css",
+                      "~/Content/css/owl.carousel.css",
+                      "~/Content/css/owl.theme.css",
+                      "~/Content/css/slitsliderStyle.css",
+                      "~/Content/css/slitsliderCustom.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -19,10 +36,22 @@ namespace HomeSharedMVC
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
         }
     }
 }
+
+    
+    //<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    //<script src="assets/bootstrap/js/bootstrap.js"></script>
+    //<script src="assets/script.js"></script>
+
+    //<!-- Owl stylesheet -->
+    //<script src="assets/owl-carousel/owl.carousel.js"></script>
+    //<!-- Owl stylesheet -->
+    
+    //<!-- slitslider -->
+    
+    //<script type="text/javascript" src="assets/slitslider/js/modernizr.custom.79639.js"></script>
+    //<script type="text/javascript" src="assets/slitslider/js/jquery.ba-cond.min.js"></script>
+    //<script type="text/javascript" src="assets/slitslider/js/jquery.slitslider.js"></script>
+    //<!-- slitslider -->
